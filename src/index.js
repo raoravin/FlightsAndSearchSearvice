9d9145f21126bba1053866fed3c255c028ac29b8
Ravin Yadav <ravinyadav973@gmail.com>
2023-05-24 11:21:12 +0530

    setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// require('dotenv').config() no need write create file on config just import
+
+
+// importing env file from config
+const { PORT } = require('./config/serverConfig');
+
+const setupAndStartServer = async () => {
+
+    //create the express object
+    const app = express();
+
+    // const PORT = 3000; no need it directly access from port variable
+    app.listen(PORT, () => {
+        console.log(`Sarver started at ${PORT}`);
+        // console.log(process.env); np need to print it just for checking
+    })
+}
+
+
+setupAndStartServer();
