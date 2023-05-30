5cc35672b46d5fd2b9d4fa976c63e8f8713f4e87
Ravin Yadav <ravinyadav973@gmail.com>
2023-05-30 22:42:47 +0530

    Added unique constraint and started cityrepository

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 // require('dotenv').config() no need write create file on config just import
-const bodyParser = require ("body-parser");
+const bodyParser = require("body-parser");
+
 
 // importing env file from config
 const { PORT } = require('./config/serverConfig');
+
+
 
 const setupAndStartServer = async () => {
 
@@ -11,13 +14,13 @@ const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
     // const PORT = 3000; no need it directly access from port variable
     app.listen(PORT, () => {
         console.log(`Sarver started at ${PORT}`);
         // console.log(process.env); np need to print it just for checking
-    })
+    });
 }
 
 
