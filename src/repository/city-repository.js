96e28806235ea0d5e9425455235bc6ab508e9290
Ravin Yadav <ravinyadav973@gmail.com>
2023-06-23 11:43:54 +0530

    Added filter to get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -58,9 +58,20 @@ class CityRepository {
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {
         try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
             const cities = await City.findAll();
+            return cities;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw { error };
